5eafa993ba5d01ce48c0d867941e870485906002
yoonuooh <yunsk9916@gmail.com>
2024-11-13 19:07:52 +0900

    2024_11_13 19:07

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -48,6 +48,9 @@ export default function Layout() {
       await auth.signOut();
       navigate("/login");
     }
+  };
+  const goEditor = () => {
+    navigate("/editor");
   }
 
   return (
@@ -60,6 +63,11 @@ export default function Layout() {
               <path clipRule="evenodd" fillRule="evenodd" d="M17 4.25A2.25 2.25 0 0 0 14.75 2h-5.5A2.25 2.25 0 0 0 7 4.25v2a.75.75 0 0 0 1.5 0v-2a.75.75 0 0 1 .75-.75h5.5a.75.75 0 0 1 .75.75v11.5a.75.75 0 0 1-.75.75h-5.5a.75.75 0 0 1-.75-.75v-2a.75.75 0 0 0-1.5 0v2A2.25 2.25 0 0 0 9.25 18h5.5A2.25 2.25 0 0 0 17 15.75V4.25Z" />
               <path clipRule="evenodd" fillRule="evenodd" d="M1 10a.75.75 0 0 1 .75-.75h9.546l-1.048-.943a.75.75 0 1 1 1.004-1.114l2.5 2.25a.75.75 0 0 1 0 1.114l-2.5 2.25a.75.75 0 1 1-1.004-1.114l1.048-.943H1.75A.75.75 0 0 1 1 10Z" />
             </svg>
+          </MenuItem>
+          <MenuItem>
+            <button onClick={goEditor}>
+              Go to Editor
+            </button>
           </MenuItem>
         </Menu>
       </Wrapper>
